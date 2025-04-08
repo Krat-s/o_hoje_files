f0668d06fa5b19177e27410721da4ac1d04bc295
Matheus Severino <104756133+Krat-s@users.noreply.github.com>
2025-04-08 15:56:26 -0300

    teste

---
+++ preenchimento.py
@@ -0,0 +1,16 @@
+import keyboard as kb
+import time
+import pyautogui as pg
+import pandas as pd
+
+pg.PAUSE = 0.5
+pg.FAILSAFE = True
+
+time.sleep(1)
+
+# pg.press('win')
+# pg.write('Opera GX')
+# pg.press('enter')   
+# pg.press('c') #atalho para abrir menu de mensagem no gmail
+pg.write('Segue PDF completo da edição', 6.722, 'do jornal O Hoje')
+
