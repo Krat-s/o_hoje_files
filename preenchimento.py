e22c98e5cde72891584e646ad705c0b057c0c5ae
Matheus Severino <104756133+Krat-s@users.noreply.github.com>
2025-04-08 16:17:17 -0300

    Flag
    Saindo do pc do comercial.
    Primeira flag criada, vai servir como ancora para rodar os códigos.
    No código atual a parte da flag é uma tentativa de gerar os números das edições de forma automática e guardar esses números, acho que o problema vai estar na operação, tentar fazer com que ele calcule, grave, preencha e depois calcule de novo, ao invéz de calcular tudo e tentar separar

--- preenchimento.py
+++ preenchimento.py
@@ -14,3 +14,71 @@ time.sleep(1)
 # pg.press('c') #atalho para abrir menu de mensagem no gmail
 pg.write('Segue PDF completo da edição', 6.722, 'do jornal O Hoje')
 
+
+
+
+flag
+import keyboard as kb
+import time
+import pyautogui as pg
+
+
+pg.PAUSE = 0.5
+pg.FAILSAFE = True
+
+time.sleep(1) 
+# NEDICAO = pg.prompt('Qual o número da edição?')
+
+# pg.press('win')
+# pg.write('Opera GX')
+# pg.press('enter')   
+# pg.press('c') #atalho para abrir menu de mensagem no gmail
+# kb.write('Segue PDF completo da edição ')
+# kb.write(NEDICAO)
+# pg.write(' do jornal O Hoje')
+
+
+# def gerar_edicoes(inicial, quantidade):
+#     edicoes = []
+    
+#     for _ in range(quantidade):
+#         edicoes.append(inicial)
+#         inicial += 1
+    
+#     # Junta as edições de fim de semana
+#     edicoes.append(f"{inicial}-{inicial+1}")
+    
+#     return edicoes
+
+# # Exemplo de uso
+# numero_inicial = 6710
+# quantidade_semanal = 5
+
+# edicoes_geradas = gerar_edicoes(numero_inicial, quantidade_semanal)
+
+# for edicao in edicoes_geradas:
+#     print(edicao)
+
+
+
+def gerar_edicoes(inicial, quantidade):
+    edicoes = []
+    
+    for _ in range(quantidade):
+        edicoes.append(str(inicial))
+        inicial += 1
+    
+    edicoes.append(f"{inicial}-{inicial+1}")
+    
+    return edicoes
+
+# Exemplo de uso
+numero_inicial = 6710
+quantidade_semanal = 6
+
+edicoes_geradas = gerar_edicoes(numero_inicial, quantidade_semanal)
+
+# Agora, você pode pegar cada número de edição de forma isolada
+for edicao in edicoes_geradas:
+    print(f"Edição: {edicao}")
+
