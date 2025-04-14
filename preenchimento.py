89a7bfa51c3e184d2b0d5c665566fcb51c54befb
Matheus Severino <104756133+Krat-s@users.noreply.github.com>
2025-04-14 15:30:12 -0300

    Versão quase pronta. Falta adicionar os emails

--- preenchimento.py
+++ preenchimento.py
@@ -3,82 +3,37 @@ import time
 import pyautogui as pg
 import pandas as pd
 
+
 pg.PAUSE = 0.5
 pg.FAILSAFE = True
 
 time.sleep(1)
 
-# pg.press('win')
-# pg.write('Opera GX')
-# pg.press('enter')   
-# pg.press('c') #atalho para abrir menu de mensagem no gmail
-pg.write('Segue PDF completo da edição', 6.722, 'do jornal O Hoje')
-
-
-
-
-flag
-import keyboard as kb
-import time
-import pyautogui as pg
-
-
-pg.PAUSE = 0.5
-pg.FAILSAFE = True
-
-time.sleep(1) 
-# NEDICAO = pg.prompt('Qual o número da edição?')
-
-# pg.press('win')
-# pg.write('Opera GX')
-# pg.press('enter')   
-# pg.press('c') #atalho para abrir menu de mensagem no gmail
-# kb.write('Segue PDF completo da edição ')
-# kb.write(NEDICAO)
-# pg.write(' do jornal O Hoje')
-
-
-# def gerar_edicoes(inicial, quantidade):
-#     edicoes = []
-    
-#     for _ in range(quantidade):
-#         edicoes.append(inicial)
-#         inicial += 1
-    
-#     # Junta as edições de fim de semana
-#     edicoes.append(f"{inicial}-{inicial+1}")
-    
-#     return edicoes
-
-# # Exemplo de uso
-# numero_inicial = 6710
-# quantidade_semanal = 5
-
-# edicoes_geradas = gerar_edicoes(numero_inicial, quantidade_semanal)
-
-# for edicao in edicoes_geradas:
-#     print(edicao)
-
-
-
 def gerar_edicoes(inicial, quantidade):
     edicoes = []
     
     for _ in range(quantidade):
-        edicoes.append(str(inicial))
+        edicoes.append(inicial)
         inicial += 1
-    
     edicoes.append(f"{inicial}-{inicial+1}")
     
     return edicoes
 
 # Exemplo de uso
 numero_inicial = 6710
-quantidade_semanal = 6
+quantidade_semanal = 3
 
 edicoes_geradas = gerar_edicoes(numero_inicial, quantidade_semanal)
 
-# Agora, você pode pegar cada número de edição de forma isolada
+# pg.hotkey('win', '3')
+  
 for edicao in edicoes_geradas:
-    print(f"Edição: {edicao}")
-
+    pg.press('c') 
+    time.sleep(1) #atalho para abrir menu de mensagem no gmail
+    pg.hotkey('ctrl', 'shift', 'b') #Atalhgo para abrir o menu de contatos em sigilo
+    #adicionar emaisl
+    pg.press('tab') 
+    time.sleep(0.3)
+    kb.write('Segue PDF completo da edição ' + (str(edicao)) + ' do jornal O Hoje')
+    pg.press('enter')
+    pg.press('esc')
