1be6476fb9ebb570ff5164403c7190abed552f9e
Matheus Severino <104756133+Krat-s@users.noreply.github.com>
2025-04-14 16:02:19 -0300

    última, estava testando sugestão do copilot

--- preenchimento.py
+++ preenchimento.py
@@ -1,8 +1,7 @@
 import keyboard as kb
 import time
 import pyautogui as pg
-import pandas as pd
-
+import csv
 
 pg.PAUSE = 0.5
 pg.FAILSAFE = True
@@ -19,21 +18,33 @@ def gerar_edicoes(inicial, quantidade):
     
     return edicoes
 
+# Lendo e-mails do CSV
+emails = []
+with open("emails.csv", newline="", encoding="utf-8") as arquivo:
+    leitor = csv.reader(arquivo)
+    next(leitor, None)  # Ignorar cabeçalho, caso exista
+    for linha in leitor:
+        emails.append(linha[1])  # Pegando apenas a coluna de e-mail
+
 # Exemplo de uso
 numero_inicial = 6710
 quantidade_semanal = 3
 
 edicoes_geradas = gerar_edicoes(numero_inicial, quantidade_semanal)
 
-# pg.hotkey('win', '3')
-  
 for edicao in edicoes_geradas:
-    pg.press('c') 
-    time.sleep(1) #atalho para abrir menu de mensagem no gmail
-    pg.hotkey('ctrl', 'shift', 'b') #Atalhgo para abrir o menu de contatos em sigilo
-    #adicionar emaisl
-    pg.press('tab') 
+    pg.press('c')  
+    time.sleep(1)  # Atalho para abrir menu de mensagem no Gmail
+    pg.hotkey('ctrl', 'shift', 'b')  # Atalho para abrir menu de contatos em sigilo
+
+    # Adicionando os e-mails automaticamente
+    for email in emails:
+        kb.write(email + "; ")  # Adiciona os e-mails separados por ponto e vírgula
+    pg.press('enter')
+
+    # Adicionando a mensagem com a edição
+    pg.press('tab')  
     time.sleep(0.3)
-    kb.write('Segue PDF completo da edição ' + (str(edicao)) + ' do jornal O Hoje')
+    kb.write(f"Segue PDF completo da edição {edicao} do jornal O Hoje")
     pg.press('enter')
     pg.press('esc')
