270408025207857848f703fa733442a28087281d
Matheus Severino <104756133+Krat-s@users.noreply.github.com>
2025-09-19 16:59:30 -0300

    ,

--- all_in_one.py
+++ all_in_one.py
@@ -1,10 +1,11 @@
 import os
 import sys
-raiz_projeto = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.append(raiz_projeto)
+# raiz_projeto = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+# sys.path.append(raiz_projeto)
 
 
-from O-HOJE-FILES.Quark_automações.Cabeçalho import abrir_software
+# from O-HOJE-FILES.Quark_automações.Cabeçalho import abrir_software
+from Quark_automações.Cabeçalho import abrir_software
 
 
 # from Gmail_automações.gmail import create_draft
