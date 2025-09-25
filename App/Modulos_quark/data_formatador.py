cd908b2bc85bcb9b48297675d33f54d440182e86
Matheus Severino <104756133+Krat-s@users.noreply.github.com>
2025-09-25 17:59:30 -0300

    Localizei o problema
    Estava com um problema na hora de printar as datas no modulos de edition_sync.py, descobri que o erro estava no formatar data, dentro de modulos quark. Ainda sem solução

--- App/Modulos_quark/data_formatador.py
+++ App/Modulos_quark/data_formatador.py
@@ -7,9 +7,9 @@ locale.setlocale(locale.LC_TIME, "pt_BR.utf8")
 # Ajustando datas
 def formatar_data(data, tipo="completo"):
     dia = int(data.strftime('%d')) 
+    dia_formatado = f"{dia}º" if dia == 1 else f"{dia}"
     proximo_dia = int((data + timedelta(days=1)).strftime('%d'))
     proximo_dia_formatado = f"{proximo_dia}º" if proximo_dia == 1 else f"{proximo_dia}"
-    dia_formatado = f"{dia}º" if dia == 1 else f"{dia}"
     mes_de_data = data.strftime('%B')
     proximo_mes = (data + timedelta(days=1)).strftime('%B')
     ano_de_data = data.strftime('%Y')
@@ -41,9 +41,12 @@ def formatar_data(data, tipo="completo"):
     if ano_de_data != virada:
         return f"Fim de semana, {dia_formatado} de dezembro de {ano_de_data} e 1º de janeiro {virada}"       
     
-    elif data.month == 3:  
+    elif data.month == 3:
         return f"{data.strftime('%A')}, {dia_formatado} de {marco_babugado} de {data.strftime('%Y')}" if data.weekday() not in formatos else formatos_marco_var[data.weekday()](data)   
-    
+
+    # elif data.weekday() != 5 and mes_de_data != proximo_mes:
+    #     return 
+
     elif mes_de_data != proximo_mes and data.month != 3:    
         return f"fim de semana, {dia_formatado} de {mes_de_data} e {proximo_dia_formatado} de {proximo_mes} de {data.strftime('%Y')}" if data.weekday() in formatos else f"{data.strftime('%A')}, {dia_formatado} de {mes_de_data} de {data.strftime('%Y')}"
     
@@ -63,7 +66,7 @@ dia_semana = hoje.now().strftime('%A')
 especifica = datetime(2029, 3, 31)
 especifica_2 = datetime(2025, 5, 31)
 teste = datetime(2028, 5, 25)
-teste_2 = datetime(2025, 12, 15)
+teste_2 = datetime(2025, 3, 15)
 teste_3 = datetime(2025, 6, 10)
 data_de_amanha = formatar_data(amanha)
 ontem = formatar_data(ontem)
@@ -74,20 +77,21 @@ teste = formatar_data(teste)
 teste_2 = formatar_data(teste_2, tipo="mes")
 teste_3 = formatar_data(teste_3, tipo="completo")
 virada_ano = formatar_data(datetime(2033, 12, 31), tipo="completo")
-teste = formatar_data()
+debug = datetime(2025, 9, 30)
+debug = formatar_data(debug)
 
 # --------------------testes---------------------
 def main():
-    print("Ontem:", ontem)
-    print("Hoje:", data_de_hoje)
-    print("Amanhã:", data_de_amanha)
-    print("virada de mar/abr:", data_especifica)
-    print("virada de ano:", virada_ano)
-    print("virada de mês random:", data_especifica_2)
-    print("tests", teste)
+    # print("Ontem:", ontem)
+    # print("Hoje:", data_de_hoje)
+    # print("Amanhã:", data_de_amanha)
+    # print("virada de mar/abr:", data_especifica)
+    # print("virada de ano:", virada_ano)
+    # print("virada de mês random:", data_especifica_2)
+    # print("tests", teste)
 
-    print(teste_3)
-    print(teste_2)
+    # print(teste_2)
+    print(debug)
 
 
 if __name__ == "__main__":  
