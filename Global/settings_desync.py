5a8aa976eeacbb40c9d982275fe82e51a198dcaa
Matheus Severino <104756133+Krat-s@users.noreply.github.com>
2025-11-25 18:02:08 -0300

    .

--- Global/settings_desync.py
+++ Global/settings_desync.py
@@ -1,3 +1,8 @@
-quantidade_repeticoes = 1
-edicao_inicial = 8001
+quantidade_repeticoes = 10
+edicao_inicial = 6965 
+
+
 total_edicoes = quantidade_repeticoes * 6
+edição_final = total_edicoes + edicao_inicial
+
+print(f"{total_edicoes} criadas, última edição: {edição_final}")
