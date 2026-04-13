83ed9e485ff16959cf11bfdcd2ec0eb571027fc1
Matheus Severino <Matheusgto02018@gmail.com>
2026-04-13 19:08:25 -0300

    .

--- Backup/run_fast.py
+++ Backup/run_fast.py
@@ -3,7 +3,16 @@ import time
 
 print('hoj')
 
+def trol():
+    time.sleep(3)
+    # pg.click(x=229, y=480)
+    pg.click(x=246, y=101)
+    time.sleep(.8)
+    pg.click(x=366, y=946)
+    pg.write('ahhhhhhhh')
+    pg.press('enter')
+    pg.hotkey('alt', '3')
 
 
-
- 
+trol()
+          
