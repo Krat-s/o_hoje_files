965771399f461a1332d926565c77431f7a17897d
Matheus Severino <104756133+Krat-s@users.noreply.github.com>
2026-09-10 15:22:28 -0300

    .

---
+++ scripts/app.py
@@ -0,0 +1,56 @@
+# age = 20
+# message = "You are an adult." if age >= 18 else "You are a minor."
+# print(message)
+
+
+# ____
+
+
+# high_income = False
+# good_credit = True
+# student = False
+
+# if (high_income or good_credit) and not student:
+#     print("Eligible for loan")
+# else:
+#     print("Not eligible for loan")
+
+
+# -----
+
+# age = 14
+# # if age in range(18, 65):
+# if 18 <= age < 65:
+#     message = "Eligible for work"
+# # else:
+# #     message = "Not eligible for work"
+# print(message)
+
+
+# ____
+
+# for number in range(1, 15, 3):
+#     print("Attempt", number, number * ".")
+
+
+# _____
+# print(range(5))
+
+# shopping_cart = ["apple", "banana", "orange"]
+# for item in shopping_cart:
+#     print(item)
+
+
+# -----
+# number = 100
+# while number > 0:
+#     print(number)
+#     number //= 2
+
+
+# ______
+# while True:
+#     command = input(">")
+#     print("ECHO", command)
+#     if command.lower() == "quit":
+#         break
