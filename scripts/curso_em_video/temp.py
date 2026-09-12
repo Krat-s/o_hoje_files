7712704d53af7288bc04e9de5341eec36ffb0fae
Matheus Severino <104756133+Krat-s@users.noreply.github.com>
2026-09-11 23:15:05 -0300

    .

--- scripts/curso_em_video/temp.py
+++ scripts/curso_em_video/temp.py
@@ -1,1 +1,11 @@
-print(4 5)
+# nome = input("Digite seu nome: ")
+# print(f"Olá, {nome}!")
+
+# idade = input("Digite sua idade: ")
+# print(f"Você tem {idade} anos.")
+
+
+n1 = float(input("Digite um número: "))
+n2 = float(input("Digite outro número: "))
+soma = n1 + n2
+print(f"A soma de {n1} e {n2} é {soma}.")
